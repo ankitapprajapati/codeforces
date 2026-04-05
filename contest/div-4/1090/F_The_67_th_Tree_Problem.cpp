9e132403e7a99b35e8841da318fb9c29a730cb62
Ankit Prajapati <ankitapprajapati6268@gmail.com>
2026-04-05 23:48:21 +0530

    day-5/30

--- contest/div-4/1090/F_The_67_th_Tree_Problem.cpp
+++ contest/div-4/1090/F_The_67_th_Tree_Problem.cpp
@@ -1,10 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int t;
     cin >> t;
-    while( t-- ){
-        
+    while (t--) {
+        long long x, y;
+        cin >> x >> y;
+
+        long long n = x + y;
+        long long d = y - x;
+
+        if ((x == 0 && n % 2 == 0) || (n / 2 < x)) {
+            cout << "NO\n";
+            continue;
+        }
+
+        cout << "YES\n";
+
+        long long mm = 2 * x + (d % 2);
+
+        for (long long i = 2; i <= mm; i++) {
+            cout << i - 1 << " " << i << "\n";
+        }
+
+        for (long long i = mm + 1; i <= n; i++) {
+            cout << mm << " " << i << "\n";
+        }
     }
+
+    return 0;
 }
